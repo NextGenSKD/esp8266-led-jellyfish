d7d8103adc5a771ac049482eecf1d72e86f3cdc1
NextGenSKD <it@ngskd.com>
2023-04-12 15:04:50 +0300

    af code

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,6 @@ struct led_RGB
   byte b;
 };
 
-
-
 byte led_tentacle = 9;
 byte led_tentacle_l = 11;
 byte led_q = 0;
@@ -23,168 +21,142 @@ int col_g = 32;
 int bright = 32;
 led_RGB scr1[12][10];
 
-
-
-
-
-
-void select_anim (byte combix)
+void debug()
 {
-          if (combix == 0) {};
-          if (combix == 1) {col_b=0;};
-          if (combix == 2) {col_r=0;};
-          if (combix == 3) {col_g=0;};
-          if (combix == 4) {col_b=0; col_r=0;};
-          if (combix == 5) {col_g=0; col_r=0;};
-          if (combix == 6) {col_b=0; col_g=0;};
-          if (combix == 7) {col_b=0; col_g=0; col_r=128;};
-          if (combix == 8) {col_b=0; col_g=128; col_r=0;};
-          if (combix == 9) {col_b=128; col_g=0; col_r=0;};
-          if (combix == 10) {col_b=128; col_g=0; col_r=128;};
-          if (combix == 11) {col_b=128; col_g=128; col_r=0;};
-          if (combix == 12) {col_b=128; col_g=0; col_r=128;};
-          if (combix == 13) {col_b=128; col_g=128; col_r=128;};
-}
-
-
-void debug()   {
-// Debug info
+  // Debug info
   Serial.print("\r Counter:");
   Serial.print(counter);
   Serial.print("   ");
   Serial.print("Led_q:");
-   Serial.print(led_q);
-   Serial.print("   ");
- 
+  Serial.print(led_q);
+  Serial.print("   ");
 
   // End Debug
-  }  
+}
 
 void pulsator()
 
 {
-    if (counter < 180)
+  if (counter < 180)
+  {
+    for (int i = 0; i <= LED_NUM; i++)
+
     {
-      for (int i=0;i<=LED_NUM;i++)
-
-      {
-          col_b = 64+sin(i*100+counter)*bright;
-          col_r = 64+cos(i*100+counter)*bright;
-          col_g = 0;
-        leds[i] = col_b+(col_r<<8)+(col_g<<16);}
+      col_b = 64 + sin(i * 100 + counter) * bright;
+      col_r = 64 + cos(i * 100 + counter) * bright;
+      col_g = 0;
+      leds[i] = col_b + (col_r << 8) + (col_g << 16);
     }
-    else {
-    //col_b = bright+sin(led_q/10)*bright;
-    //col_r = bright+cos(led_q/10)*bright;
-    //col_g = 0;
-    //select_anim(led_q);
-
-    counter = 0; 
-    if (led_q<=180) {led_q++;} else {led_q=0;}
-     }
-
+  }
+  else
+  {
+    counter = 0;
+    if (led_q <= 180)
+    {
+      led_q++;
+    }
+    else
+    {
+      led_q = 0;
+    }
+  }
 }
 void roller()
 
 {
-//print_pixels
-byte rnd = random(10);
-int x = 0;
-int y = random(led_tentacle);
-if (rnd==1) {
-       scr1[x][y].b = 120;
-       scr1[x][y].r = 120;
-       scr1[x][y].g = 0;
-} 
-if (rnd==2) {
-       scr1[x][y].b = 120;
-       scr1[x][y].r = 80;
-       scr1[x][y].g = 0;
-}
+  // print_pixels
+  byte rnd = random(10);
+  int x = 0;
+  int y = random(led_tentacle);
+  if (rnd == 1)
+  {
+    scr1[x][y].b = 120;
+    scr1[x][y].r = 120;
+    scr1[x][y].g = 0;
+  }
+  if (rnd == 2)
+  {
+    scr1[x][y].b = 120;
+    scr1[x][y].r = 80;
+    scr1[x][y].g = 0;
+  }
 
-if (rnd==3) {
-       scr1[x][y].b = 80;
-       scr1[x][y].r = 120;
-       scr1[x][y].g = 0;
-}
-  
- // scroll
+  if (rnd == 3)
+  {
+    scr1[x][y].b = 80;
+    scr1[x][y].r = 120;
+    scr1[x][y].g = 0;
+  }
 
-for (int x=led_tentacle_l;x>0;x--)
+  // scroll
+
+  for (int x = led_tentacle_l; x > 0; x--)
+  {
+
+    for (int y = 0; y <= led_tentacle; y++)
     {
-      
-      for (int y=0;y<=led_tentacle;y++)
-      { 
-       byte led_r = scr1[x-1][y].r;
-       byte led_g = scr1[x-1][y].g;
-       byte led_b = scr1[x-1][y].b;   
-       scr1[x][y].b = led_b;
-       scr1[x][y].r = led_r;
-       scr1[x][y].g = led_g;
-        if (led_r > 20) led_r-=20; else led_r=0;
-        if (led_g > 20) led_g-=20; else led_g=0;
-        if (led_b > 40) led_b-=20; else led_b=20;
-       scr1[x-1][y].b = led_b;
-       scr1[x-1][y].r = led_r;
-       scr1[x-1][y].g = led_g;
-       
-        }
-    
+      byte led_r = scr1[x - 1][y].r;
+      byte led_g = scr1[x - 1][y].g;
+      byte led_b = scr1[x - 1][y].b;
+      scr1[x][y].b = led_b;
+      scr1[x][y].r = led_r;
+      scr1[x][y].g = led_g;
+      if (led_r > 20)
+        led_r -= 20;
+      else
+        led_r = 0;
+      if (led_g > 20)
+        led_g -= 20;
+      else
+        led_g = 0;
+      if (led_b > 40)
+        led_b -= 20;
+      else
+        led_b = 20;
+      scr1[x - 1][y].b = led_b;
+      scr1[x - 1][y].r = led_r;
+      scr1[x - 1][y].g = led_g;
     }
+  }
 
-
-//print_to_led
-for (int y=0;y<=led_tentacle;y++) 
-      {
-        for (int x=0;x<=led_tentacle_l;x++)
-        {
-          leds[y*led_tentacle+x] = ((scr1[x][y].b)+(scr1[x][y].r<<8)+(scr1[x][y].g<<16));
-        }
-
+  // print_to_led
+  for (int y = 0; y <= led_tentacle; y++)
+  {
+    for (int x = 0; x <= led_tentacle_l; x++)
+    {
+      leds[y * led_tentacle + x] = ((scr1[x][y].b) + (scr1[x][y].r << 8) + (scr1[x][y].g << 16));
+    }
+  }
 }
 
-
-
-
-
-}
-
-void setup() {
+void setup()
+{
   Serial.begin(921600);
   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, LED_NUM);
   FastLED.setBrightness(20);
   Serial.println("\r SETUP Success!");
- //init masive
-for (int x=0; x<=led_tentacle_l; x++) 
-      {
-        for (int y=0;y<=led_tentacle; y++)
-        {
-          scr1[x][y].r = 0;
-          scr1[x][y].g = 0;
-          scr1[x][y].b = 20;
-        }
-
-      }
-
-
+  // init masive
+  for (int x = 0; x <= led_tentacle_l; x++)
+  {
+    for (int y = 0; y <= led_tentacle; y++)
+    {
+      scr1[x][y].r = 0;
+      scr1[x][y].g = 0;
+      scr1[x][y].b = 20;
+    }
+  }
 }
 
-void loop() {
+void loop()
+{
 
+  // pulsator();
 
-  //pulsator();
-
-
-      
-//for (byte i=0; i<=led_tentacle_l;i++)
- roller();
-
+  roller();
 
   FastLED.show();
   delay(50);
   debug();
-  //FastLED.clear();
+  // FastLED.clear();
   counter++;
-  
-
 }
